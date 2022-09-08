21e545569d2932616b0662d2e7294e73054faa21
Garmanov Kostya <napalmcatt@mail.ru>
2022-09-08 16:22:54 +0700

    Add function integral_view

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 
+def integral_view(image: list[list]) -> list[list]:
+    integral_matrix = image
+    integral_element(len(integral_matrix)-1, len(integral_matrix[0])-1, image, integral_matrix)
+    return integral_matrix
+
+
+def integral_element(i, j, matrix, integral_matrix):
+    if i == 0 and j == 0:
+        integral_matrix[i][j] = matrix[i][j]
+    elif i == 0:
+        integral_matrix[i][j] = matrix[i][j] + integral_element(i, j-1, matrix, integral_matrix)
+    elif j == 0:
+        integral_matrix[i][j] = matrix[i][j] + integral_element(i-1, j, matrix, integral_matrix)
+    else:
+        integral_matrix[i][j] = matrix[i][j] - integral_element(i-1, j-1, matrix, integral_matrix) +\
+                                integral_element(i, j-1, matrix, integral_matrix) +\
+                                integral_element(i-1, j, matrix, integral_matrix)
+    return integral_matrix[i][j]
+
+
+if __name__ == "__main__":
+    input_matrix = [[1, 2], [1, 0]]
+    print(integral_view(input_matrix))
