26e5103239f107512942ebd5b142067fb809f4f3
Garmanov Kostya <napalmcatt@mail.ru>
2022-09-11 15:59:21 +0700

    Add rect_summ function

--- main.py
+++ main.py
@@ -22,6 +22,22 @@ def integral_element(i, j, matrix, integral_matrix):
     return integral_matrix[i][j]
 
 
+def rect_sum(image: list[list], x1: int, y1: int, x2: int, y2: int) -> int:
+    integral_matrix = integral_view(image)
+    if x1 >= x2 or y1 >= y2 or x1 < 0 or y1 < 0:
+        return 0
+    elif x1 == 0 and y1 == 0:
+        return integral_matrix[y2 - 1][x2 - 1]
+    elif x1 == 0:
+        return integral_matrix[y2 - 1][x2 - 1] - integral_matrix[y1-1][x2-1]
+    elif y1 == 0:
+        return integral_matrix[y2 - 1][x2 - 1] - integral_matrix[y2-1][x1-1]
+    else:
+        return integral_matrix[y2-1][x2-1] + integral_matrix[y1-1][x1-1] - \
+               integral_matrix[y2-1][x1-1] - integral_matrix[y1-1][x2-1]
+
+
 if __name__ == "__main__":
-    input_matrix = [[1, 2], [1, 0]]
+    input_matrix = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
     print(integral_view(input_matrix))
+    print(rect_sum(input_matrix, 1, 0, 2, 3))
